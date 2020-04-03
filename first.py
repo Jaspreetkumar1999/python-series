4f657f2038087949d74494eb47fe36695fcedee3
Jaspreetkumar1999 <60319195+Jaspreetkumar1999@users.noreply.github.com>
2020-04-03 08:31:53 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,34 @@
+# message ='jassi's world'
+# variable name should be without space it should be good if it in discriptive mannee
+# there is no convention about the use of single quotes and double quotes you can use any one but some time for eg 'jassi's car' this will going to creat problem so better to use double quotes
+# message ='jassi\'s world'
+# for string containing multiple quotes inside it use pair of 3 double  quotes
+# message = """ here is jassi's dog. it is a street dog adopted by him. I'ld like to call him """
+message ="Hello world"
+# print(len(message))  to print the length of our string also count the space
+# print(message[10]) to print  charachter stored at the particular index
+# print(message[2:7]) to print characters of partivular range
+# print(message[:7]) this will take starting point as begining
+# print(message[2:]) to print evevry character after the 2nd position end at last character
+# print(message.lower()) to convert whole string into lowercase
+# print(message.upper()) to convert whole string into uppercase
+# print(message.count('l')) to count how many times character or string appears in our input
+# print(message.find('l')) to print starting index of character
+# print(message.find('jass')) it will return -1 bcz there is no matching character
+# new_messge = message.replace('world','jasprert')
+# print(new_messge) to replace we must have to creat new variable which will store our replaced version of old one
+boy ="great"
+# combine =message+boy
+# # print(combine)  to concatenation two string without space
+# combine =message+' '+ boy
+# print(combine)print(combine)  to concatenation two string with space
+# combine = '{}, {}. welcome!'.format(message,boy)
+# print(combine) to concatenation with efficent way
+# combine = f'{message}, {boy}. welcome!'same as previous
+# combine = f'{message}, {boy.upper()}. welcome!' to convert particular string in uppercase
+# print(dir(boy)) it will show whole operation and methods which we can perform on our variable
+# print(help(str)) it will give documentation regarding strimg
+# print(help(str.upper)) it will give us  documentation of particular method
+
+
+
